fb495e66a2f4ea94534905d660e59234890317d4
jafan-mulama <jafan2222@gmail.com>
2023-06-12 22:12:25 +0300

    Initial commit

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,23 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observer } from 'rxjs';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.scss']
 })
-export class PostsComponent {
+export class PostsComponent implements OnInit {
   posts: any[] = [];
   newPost: any = {};
   private apiUrl: string = 'https://jsonplaceholder.typicode.com/posts';
   message!: string;
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
     this.getPosts();
   }
 
-  getPosts() {
+  getPosts(): void {
     this.http.get<any[]>(this.apiUrl)
       .subscribe({
         next: (response) => {
@@ -30,31 +31,29 @@ export class PostsComponent {
       });
   }
 
-  createPost(idInput: HTMLInputElement, titleInput: HTMLInputElement, bodyInput: HTMLInputElement): void | string  {
+  createPost(idInput: HTMLInputElement, titleInput: HTMLInputElement, bodyInput: HTMLInputElement): void {
     if (!idInput.value || !titleInput.value || !bodyInput.value) {
       // One or more input fields are empty, do not proceed with the submission
       this.message = 'One or more input fields are empty. Please fill all fields.';
       return;
     }
-    
+
     let newPost = {
       id: idInput.value,
       title: titleInput.value,
       body: bodyInput.value
     };
 
-    idInput.value ='';
-    titleInput.value ='';
-    bodyInput.value ='';
+    idInput.value = '';
+    titleInput.value = '';
+    bodyInput.value = '';
 
     this.http.post<any[]>(this.apiUrl, newPost)
       .subscribe({
         next: (response: any) => {
           console.log(response);
-          newPost.id = newPost.id;
-          newPost.title = response.title;
-          newPost.body = response.body;
-          this.posts.splice(0, 0, newPost);
+          newPost.id = response.id;
+          this.posts.unshift(newPost);
           this.clearForm();
         },
         error: (error) => {
@@ -67,39 +66,39 @@ export class PostsComponent {
     const id = Number(idInput.value);
     const title = titleInput.value;
     const body = bodyInput.value;
-  
-    // Rest of your code here...
-  
-    // Update the post object with the new values
+
     post.id = id;
     post.title = title;
     post.body = body;
-  
-    // Rest of your HTTP request code here...
+
+    this.http.put<any>(this.apiUrl + '/' + post.id, post)
+      .subscribe({
+        next: (response: any) => {
+          console.log(response);
+        },
+        error: (error: any) => {
+          console.error(error);
+        }
+      });
   }
-  
-  
-  
-  
 
   deletePost(post: any): void {
-    this.http.delete(this.apiUrl+ '/' + post.id).subscribe({
+    this.http.delete(this.apiUrl + '/' + post.id).subscribe({
       next: (response: any) => {
         console.log(response);
-        let indext = this.posts.indexOf(post);
-        this.posts.splice(indext, 1)
-    
+        const index = this.posts.indexOf(post);
+        if (index !== -1) {
+          this.posts.splice(index, 1);
+        }
       },
       error: (error: any) => {
         console.error(error);
-        // Handle the error here, display error message, etc.
       },
       complete: () => {
         // Optional: Handle completion of the observable
       }
     });
   }
-  
 
   clearForm() {
     this.newPost = {};
